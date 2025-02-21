62ab61d60df2f8f77d7e7a46670829372afbde6f
Gregor Eichelberger <gregor.eichelberger@tuwien.ac.at>
2025-02-21 21:53:21 +0100

    shio: Backport #6498: Optimize search

--- modules/search-service-impl/src/main/java/org/opencastproject/search/endpoint/SearchRestService.java
+++ modules/search-service-impl/src/main/java/org/opencastproject/search/endpoint/SearchRestService.java
@@ -46,6 +46,7 @@ import com.google.gson.Gson;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
+import org.elasticsearch.index.query.Operator;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
@@ -340,9 +341,9 @@ public class SearchRestService extends AbstractJobProducerEndpoint {
     List<String> series = Collections.emptyList();
     if (StringUtils.isNotEmpty(seriesName)) {
       var seriesSearchSource = new SearchSourceBuilder().query(QueryBuilders.boolQuery()
-          .must(QueryBuilders.termQuery(SearchResult.ORG, org))
-          .must(QueryBuilders.termQuery(SearchResult.TYPE, SearchService.IndexEntryType.Series))
-          .must(QueryBuilders.termQuery(SearchResult.DUBLINCORE + ".title", seriesName))
+          .filter(QueryBuilders.termQuery(SearchResult.ORG, org))
+          .filter(QueryBuilders.termQuery(SearchResult.TYPE, SearchService.IndexEntryType.Series))
+          .filter(QueryBuilders.termQuery(SearchResult.DUBLINCORE + ".title", seriesName))
           .mustNot(QueryBuilders.existsQuery(SearchResult.DELETED_DATE)));
       series = searchService.search(seriesSearchSource).getHits().stream()
           .map(h -> h.getDublinCore().getFirst(DublinCore.PROPERTY_IDENTIFIER))
@@ -354,12 +355,12 @@ public class SearchRestService extends AbstractJobProducerEndpoint {
     }
 
     var query = QueryBuilders.boolQuery()
-        .must(QueryBuilders.termQuery(SearchResult.ORG, org))
-        .must(QueryBuilders.termQuery(SearchResult.TYPE, type))
+        .filter(QueryBuilders.termQuery(SearchResult.ORG, org))
+        .filter(QueryBuilders.termQuery(SearchResult.TYPE, type))
         .mustNot(QueryBuilders.existsQuery(SearchResult.DELETED_DATE));
 
     if (StringUtils.isNotEmpty(id)) {
-      query.must(QueryBuilders.idsQuery().addIds(id));
+      query.filter(QueryBuilders.idsQuery().addIds(id));
     }
 
     if (StringUtils.isNotEmpty(seriesId)) {
@@ -378,7 +379,12 @@ public class SearchRestService extends AbstractJobProducerEndpoint {
     }
 
     if (StringUtils.isNotEmpty(text)) {
-      query.must(QueryBuilders.matchQuery("fulltext", text));
+      query.minimumShouldMatch(1);
+      query.should(
+          QueryBuilders.matchQuery("fulltext", text)
+              .fuzziness("AUTO")
+              .operator(Operator.AND)
+      );
     }
 
     var user = securityService.getUser();
