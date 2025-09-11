fe6a4ef42118f24c14595bc08b1dd6a31394aaaa
Lars Kiesow <lkiesow@uos.de>
2025-09-11 19:23:46 +0200

    shio: Backport #7048: Switch to static reference
    Using dynamic references is usually not a good idea unless you really
    need them since the service you reference can potentially not exist
    which means your reference is `null`. That's something you have to check
    everywhere.
    
    Opencast clearly doesn't do that, resulting in errors like this:
    
    ```
    2025-09-11T06:21:19,711 | WARN  | (IngestRestService:926) - Unable to add mediapackage
    java.lang.NullPointerException: Cannot invoke "org.opencastproject.list.api.ListProvidersService.hasProvider(String)" because "listProvidersService" is null
            at org.opencastproject.elasticsearch.index.objects.event.EventIndexUtils.addAuthorization(EventIndexUtils.java:393) ~[?:?]
            at org.opencastproject.elasticsearch.index.objects.event.EventIndexUtils.toSearchMetadata(EventIndexUtils.java:249) ~[?:?]
            at org.opencastproject.elasticsearch.index.ElasticsearchIndex.update(ElasticsearchIndex.java:370) ~[?:?]
            at org.opencastproject.elasticsearch.index.ElasticsearchIndex.addOrUpdateEvent(ElasticsearchIndex.java:348) ~[?:?]
            at org.opencastproject.assetmanager.impl.AssetManagerImpl.updateEventInIndex(AssetManagerImpl.java:585) ~[?:?]
            at org.opencastproject.assetmanager.impl.AssetManagerImpl.takeSnapshot(AssetManagerImpl.java:441) ~[?:?]
            at org.opencastproject.assetmanager.util.WorkflowPropertiesUtil.storeProperties(WorkflowPropertiesUtil.java:118) ~[?:?]
            at org.opencastproject.workflow.impl.WorkflowServiceImpl.start(WorkflowServiceImpl.java:502) ~[?:?]
            at org.opencastproject.workflow.impl.WorkflowServiceImpl.start(WorkflowServiceImpl.java:477) ~[?:?]
            at org.opencastproject.ingest.impl.IngestServiceImpl.ingest(IngestServiceImpl.java:1287) ~[?:?]
            at org.opencastproject.ingest.impl.IngestServiceImpl.ingest(IngestServiceImpl.java:1217) ~[?:?]
            at org.opencastproject.ingest.endpoint.IngestRestService.addMediaPackage(IngestRestService.java:917) ~[?:?]
            …
    ```
    
    Looking into this, it seems like this dynamic reference isn't actually
    necessary. Therefor, let's turn this into a static reference.

--- modules/elasticsearch-index/src/main/java/org/opencastproject/elasticsearch/index/ElasticsearchIndex.java
+++ modules/elasticsearch-index/src/main/java/org/opencastproject/elasticsearch/index/ElasticsearchIndex.java
@@ -56,9 +56,6 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
-import org.osgi.service.component.annotations.ReferencePolicyOption;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -120,11 +117,7 @@ public class ElasticsearchIndex extends AbstractElasticsearchIndex {
 
   private ListProvidersService listProvidersService;
 
-  @Reference(
-      cardinality = ReferenceCardinality.OPTIONAL,
-      policy = ReferencePolicy.DYNAMIC,
-      policyOption = ReferencePolicyOption.GREEDY
-  )
+  @Reference
   public void setListProvidersService(ListProvidersService listProvidersService) {
     this.listProvidersService = listProvidersService;
   }
