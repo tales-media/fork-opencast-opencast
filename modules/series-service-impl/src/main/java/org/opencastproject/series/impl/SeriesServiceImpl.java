230b449df5933d00b1a6a6a973529dc43617fbbd
Matthias Neugebauer <mtneug@shio.solutions>
2025-09-14 15:07:21 +0200

    shio: Make sure a license is set on a series

--- modules/series-service-impl/src/main/java/org/opencastproject/series/impl/SeriesServiceImpl.java
+++ modules/series-service-impl/src/main/java/org/opencastproject/series/impl/SeriesServiceImpl.java
@@ -104,6 +104,8 @@ public class SeriesServiceImpl extends AbstractIndexProducer implements SeriesSe
 
   private static final String THEME_PROPERTY_NAME = "theme";
 
+  private static final String TALES_MEDIA_DEFAULT_SERIES_LICENSE = "ALLRIGHTS";
+
   /** Persistent storage */
   protected SeriesServiceDatabase persistence;
 
@@ -184,6 +186,11 @@ public class SeriesServiceImpl extends AbstractIndexProducer implements SeriesSe
       if (dublinCoreOpt.isPresent()) {
         DublinCoreCatalog dublinCore = dublinCoreOpt.get();
         final String id = dublinCore.getFirst(DublinCore.PROPERTY_IDENTIFIER);
+
+        if (!dublinCore.hasValue(DublinCore.PROPERTY_LICENSE)) {
+          dublinCore.set(DublinCore.PROPERTY_LICENSE, TALES_MEDIA_DEFAULT_SERIES_LICENSE);
+          logger.debug("Setting series default license to '{}'", TALES_MEDIA_DEFAULT_SERIES_LICENSE);
+        }
 
         if (!dublinCore.hasValue(DublinCore.PROPERTY_CREATED)) {
           DublinCoreValue date = EncodingSchemeUtils.encodeDate(new Date(), Precision.Minute);
